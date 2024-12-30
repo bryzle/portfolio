853f2479318fd7b519564ad0b13815c0c175244f
bryzle <brandonlum17@gmail.com>
2024-12-30 11:46:56 -0500

    initialize React project with Vite and create basic component structure

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import About from '../components/about/About'
+import Blog from '../components/blog/Blog'
+import Contact from '../components/contact/Contact'
+import Home from '../components/home/Home'
+import Portfolio from '../components/portfolio/Portfolio'
+import Pricing from '../components/pricing/Pricing'
+import Resume from '../components/resume/Resume'
+import Services from '../components/services/Services'
+import Sidebar from '../components/sidebar/Sidebar'
+import Testimonial from './components/testimonial/Testimonial'
+
+function App() {
+
+  return (
+    <>
+      <About />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/blog/Blog.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import './blog.css'
+
+const blog = () => {
+(
+    <div>blog</div>
+  )
+}
+
+export default blog
